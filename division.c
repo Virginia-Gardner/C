cd94b5d9a3f29907f33ec5800e3e5cdf7fab1070
Rustem Khusnutdinov <ronald@Rustems-MacBook-Air.local>
2021-08-10 11:16:26 +0300

    Deleted some not mine files

--- division.c
+++ division.c
@@ -3,11 +3,11 @@
 int	main(int argc, char* argv[])
 {
 	int x = 12345;
-	/* %d - digit, \n - перевод каретки*/
-	while (x != 0){
-	printf(%d %d\n, x/10, x%10);
-	x /= 10;
+	while (x != 0)
+	{
+		printf(%d %d\n, x/10, x%10);
+		x /= 10;
 	}
-	double x = 1./2. /* 1. - так мы указываем что число не int */
+	double x = 1./2.
 	return 0;
 }
